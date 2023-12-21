d5dcd3131de98e3a457c76f4eaa293dd89b875f5
DarioLoForti <lofortidario@gmail.com>
2023-12-21 15:46:13 +0100

    bonus completed

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data(){
+        return{
+            image: "vue.svg"
+        }
+    },
+    methods: {
+        getIamge(path){
+            return new URL(path, import.meta.url).href
+        }
+    },
+}
+</script>
+
+<template lang="">
+    <div>
+        <img :src="getIamge(`../assets/${image}`)" width="150px">
+    </div>
+</template>
+<style lang="">
+    
+</style>
